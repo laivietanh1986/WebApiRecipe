00755b513ac39e84fa50906db53d239afd9f51a0
laivietanh1986 <laivietanh1986@gmail.com>
2017-04-08 10:40:03 +0700

    Add project files.

---
+++ WebApiRecipe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiRecipe.MvcApplication" Language="C#" %>
---
+++ WebApiRecipe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApiRecipe.Startup))]
+namespace WebApiRecipe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
